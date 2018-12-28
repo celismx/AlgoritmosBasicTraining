877f5cafaffc4033f194085c9dfe1264f4404946
celismx <celismx@gmail.com>
2018-12-28 16:42:47 -0500

    final final del bubblesort

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,6 +1,6 @@
-1.- Comenzamos a hacer la comparación de elementos adyacentes
+/* 1.- Comenzamos a hacer la comparación de elementos adyacentes
 2.- repetimos hasta tener una pasada completa sin ningún swap
-
+ */
 #include<stdio.h>
 
 void cambiar_pos(int *n1, int *n2)
@@ -10,7 +10,7 @@ void cambiar_pos(int *n1, int *n2)
     *n2 = temp;
 }
 
-void bubbleSort(int vector_entrada[], n)
+void bubbleSort(int vector_entrada[], int n)
 {
     int i, j;
     for(i=0; i < n-1; i++)
@@ -22,4 +22,22 @@ void bubbleSort(int vector_entrada[], n)
             cambiar_pos(&vector_entrada[j],&vector_entrada[j+1]);
         }
     }
+}
+
+int print_array(int vector_entrada[], int n)
+{
+    int i;
+    for(i=0; i<n-1; i++)
+        printf("%d ,", vector_entrada[i]);
+    printf("\n fin del ordenamiento");
+}
+
+main(int argc, char const *argv[])
+{
+    int vector_entrada[]={100, 1992, 0, 5, -1, 60, 70, 14, 15, 10};
+    int n = sizeof(vector_entrada)/sizeof(vector_entrada[0]);
+    bubbleSort(vector_entrada, n);
+    print_array(vector_entrada, n);
+    printf("\n");
+    return 0;
 }
