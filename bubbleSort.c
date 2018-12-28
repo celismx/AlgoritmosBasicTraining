5fe6f80102a0c7f826ce6f0946d18b259bdfa694
celismx <celismx@gmail.com>
2018-12-28 10:00:34 -0500

    bubble sort

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,5 +1,25 @@
-/* 1.- Comenzamos a hacer la comparación de elementos adyacentes
-2.- repetimos hasta tener una pasada completa sin ningún swap */
+1.- Comenzamos a hacer la comparación de elementos adyacentes
+2.- repetimos hasta tener una pasada completa sin ningún swap
 
 #include<stdio.h>
 
+void cambiar_pos(int *n1, int *n2)
+{
+    int temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
+
+void bubbleSort(int vector_entrada[], n)
+{
+    int i, j;
+    for(i=0; i < n-1; i++)
+    {
+        for(j=0; j < n-i-1; j++)
+        {
+            //[10, 5, 3, 3333]
+            if(vector_entrada[j]>vector_entrada[j+1])
+            cambiar_pos(&vector_entrada[j],&vector_entrada[j+1]);
+        }
+    }
+}
